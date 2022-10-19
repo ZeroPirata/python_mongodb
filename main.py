35b7675ed2d8e024fc2d9805074721e379d13323
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-19 17:52:23 -0300

    Redis adicionado

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 # https://www.w3schools.com/python/default.asp
 # https://medium.com/analytics-vidhya/crud-operations-in-mongodb-using-python-49b7850d627e
 # https://www.mongodb.com/docs/manual/reference/operator/update/addToSet/
+# Redis
+# https://redis-py.readthedocs.io/en/stable/examples/search_json_examples.html
+# https://stackoverflow.com/questions/64067197/python3-redis-redis-exceptions-responseerror-unknown-command-json-set
+# https://koalatea.io/python-redis-hash/
+# https://redis.io/commands/set/
+from base64 import decode
+from genericpath import exists
+import os
 import pymongo
 from pymongo.server_api import ServerApi
 from bson.objectid import ObjectId
+import redis
+import json
 
+clearConsole = lambda: os.system('cls'
+                                 if os.name in ('nt', 'dos') else 'clear')
+conR = redis.Redis(host='redis-13542.c275.us-east-1-4.ec2.cloud.redislabs.com',
+                   port=13542,
+                   password='qR58kVZOGUkWSQ5M5WTs3bCZ3mwMAujk')
 client = pymongo.MongoClient(
     "mongodb+srv://zeropirata:123@atlascluster.djqn7mq.mongodb.net/?retryWrites=true&w=majority",
     server_api=ServerApi('1'))
@@ -52,7 +67,7 @@ def insertProduto(nome, preco, descricao, vendedor):
 def insertUsuario(nome, email):
     global mydb
     mycol = mydb.usuario
-    mydict = {"nome": nome, "email": email}
+    mydict = {"nome": nome, "email": email, "favorito": []}
     x = mycol.insert_one(mydict)
     print(x.inserted_id)
 
@@ -118,7 +133,6 @@ def updateClienteFavoritos(idAlvo, idProduto):
     columnProduto = mydb.produto
     columnUser = mydb.usuario
     objInstance = ObjectId(idProduto)
-    myquery = {"_id": objInstance}
     findProdutos = columnProduto.find({"_id": objInstance}, {
         "_id": 1,
         "nome": 1,
@@ -269,6 +283,17 @@ def findQueryVendedor(alvo):
         print(result)
 
 
+def findSortUserRedis():
+    global mydb
+    userColumn = mydb.usuario
+    mydoc = userColumn.find({}, {
+        "nome": 1,
+        "_id": 0,
+    }).sort("nome")
+    for result in mydoc:
+        print(result)
+
+
 def findQueryCompras(alvo):
     global mydb
     comprasColumn = mydb.compra
@@ -278,32 +303,152 @@ def findQueryCompras(alvo):
         print(result)
 
 
+# Redis
+
+
+def getAlvoRedis(alvo):
+    global mydb
+    mycol = mydb.usuario
+    myquery = {"nome": {"$eq": alvo}}
+    mydoc = mycol.find(myquery)
+    createDic = {}
+    dados = conR.keys()
+    usuarios = []
+    for result in mydoc:
+        createDic.update(result)
+        createDic.pop("nome")
+        createDic.pop("_id")
+    x = 0
+    for usuariosRedis in dados:
+        usuarios.append(usuariosRedis.decode())
+    user = 'user:' + createDic['email']
+    if len(dados) == 0:
+        conR.hset('fav:' + createDic["email"], str('favoritos'),
+                  json.dumps(createDic["favorito"]))
+        conR.hset('user:' + createDic["email"], str('loggin'), 'off')
+        print('Usuario Criado com sucesso!')
+    else:
+        if user in usuarios:
+            print(f"O usuario: {alvo}. Já está inserido dentro do redis...")
+        else:
+            conR.hset('fav:' + createDic["email"], str('favoritos'),
+                      json.dumps(createDic["favorito"]))
+            conR.hset('user:' + createDic["email"], str('loggin'), 'off')
+            print("Inserido com sucesso")
+
+
+def adicionarFavoritoRedis(alvo, idProduto):
+    columnProduto = mydb.produto
+    userFavoritos = json.loads(conR.hget(f'fav:' + alvo, 'favoritos'))
+    objInstance = ObjectId(idProduto)
+    arcane = []
+    findProdutos = columnProduto.find({"_id": objInstance}, {
+        "_id": 0,
+        "nome": 1,
+        "preco": 1
+    })
+    for redisFavoritos in userFavoritos:
+        arcane.append(redisFavoritos)
+    for produto in findProdutos:
+        arcane.append(produto)
+        conR.hset(f'fav:{alvo}', 'favoritos', json.dumps(arcane))
+
+
+def sincronizarRedisMongo(alvo):
+    global mydb
+    columnUser = mydb.usuario
+    myquery = {"email": {"$eq": alvo}}
+    userFavoritos = json.loads(conR.hget(f'fav:' + alvo, 'favoritos'))
+    arcane = []
+    for redisFavoritos in userFavoritos:
+        arcane.append(redisFavoritos)
+    for misftToys in arcane:
+        insertFavorito = {"$addToSet": {"favorito": misftToys}}
+        columnUser.update_one(myquery, insertFavorito)
+    print("Sincronizado com sucesso")
+
+
+def listKeys():
+    dados = conR.keys()
+    for usuariosRedis in dados:
+        decodedObj = usuariosRedis.decode()
+        if decodedObj.__contains__('user:'):
+            print(decodedObj.replace('user:', 'Email: '))
+
+
+def listFavoritosUser(alvo):
+    dados = conR.hget(f'fav:bicho@nunes', 'favoritos')
+    print(dados)
+
+
+def removerUser(alvo):
+    conR.delete('fav:' + alvo)
+    conR.delete('user:' + alvo)
+    print('Removido com sucesso')
+
+
+def loginRedis(alvo):
+    login = f'logged:{alvo}'
+    timeToLeft = conR.ttl(login)
+    if timeToLeft <= -1:
+        print("Deslogdado...")
+        conR.hset(f'logged:{alvo}', 'timeExit', 0)
+        conR.hset(f'user:{alvo}', 'loggin', 'off')
+        efetuarLogin = input("Deseja logar? S/N? ")
+        if efetuarLogin == 'S':
+            conR.expire('logged:bicho@nunes', 15)
+        else:
+            print("Saindo...")
+    else:
+        print(
+            f"Você já está logado...\nVocê tem {conR.ttl(login)}s segundos para ser deslogado..."
+        )
+        conR.hset(f'logged:{alvo}', 'loggin', 'on')
+
+
 def main():
-    print("""
-    1 - Insert de Produto \n 
-    2 - Insert de Usuario \n
-    3 - Insert de Vendedor \n
-    4 - Update dos Dados do Cliente \n
-    5 - Colocar Itens nos Favoritos \n
-    6 - Listagem dos Usuarios \n
-    7 - Listagem dos Produtos \n
-    8 - Listagem dos Vendedores \n
-    9 - Listagem das Compras \n
-    10 - Achar Usuario \n
-    11 - Achar Produto \n
-    12 - Achar Vendedor \n
-    13 - Achar Compra pelo usuario \n
-    14 - Fazer uma compra \n
-    15 - Deletar Usuario \n
-    16 - Deletar Produto \n
-    17 - Deletar Vendedor \n
-    18 - Deletar Compra \n
-    19 - Update Vendedor \n
-    20 - Update Produto \n
-    X - Fechar \n
-    """)
+    clearConsole()
+    print(""" 
+        1 - Mongo
+        2 - Redis 
+     """)
+    select = input("Qual opção deseja?: ")
+    if select == '1':
+        mongo()
+    elif select == '2':
+        redisAtv()
+    else:
+        print("Opção Não entendida")
+
+
+def mongo():
+    clearConsole()
     on = True
     while on:
+        print("""
+        1 - Insert de Produto \n 
+        2 - Insert de Usuario \n
+        3 - Insert de Vendedor \n
+        4 - Update dos Dados do Cliente \n
+        5 - Colocar Itens nos Favoritos \n
+        6 - Listagem dos Usuarios \n
+        7 - Listagem dos Produtos \n
+        8 - Listagem dos Vendedores \n
+        9 - Listagem das Compras \n
+        10 - Achar Usuario \n
+        11 - Achar Produto \n
+        12 - Achar Vendedor \n
+        13 - Achar Compra pelo usuario \n
+        14 - Fazer uma compra \n
+        15 - Deletar Usuario \n
+        16 - Deletar Produto \n
+        17 - Deletar Vendedor \n
+        18 - Deletar Compra \n
+        19 - Update Vendedor \n
+        20 - Update Produto \n
+        X - Fechar \n
+        cls - Clear Console
+        """)
         select = input("Qual opção deseja?: ")
         if select == "1":
             nomeProduto = input("Nome do produto: \n ")
@@ -384,8 +529,126 @@ def main():
             novoPreco = input("Digite o novo preço: R$")
             novaDesc = input("Nova descrição: \n")
             updateProduto(alvo, novoNome, novoPreco, novaDesc)
+        elif select == "cls":
+            clearConsole()
+            return mongo()
         elif select == "X":
             on = False
+            main()
+
+
+def redisAtv():
+    clearConsole()
+    on = True
+    while on:
+        print("""
+            1 - Inserir Usuario no Redis
+            2 - Adicionar item no Favorito
+            3 - Listagem dos Usuarios já pertencentes Redis
+            4 - Remover Usuario do Redis
+            5 - Listar itens no favorito
+            6 - Sincronizar Usuario no Mongo
+            7 - Login Redis
+            X - Fechar
+            cls - Clear console
+        """)
+        select = input("Qual opção deseja?: ")
+        if select == '1':
+            findSortUserRedis()
+            alvo = input("Insira o nome da pessoa: ")
+            getAlvoRedis(alvo)
+        elif select == '2':
+            listKeys()
+            alvo = input("Insira o Email da pessoa: ")
+            findSortProduto()
+            produtoAlvo = input("Escolha o produto pelo ID: \n")
+            adicionarFavoritoRedis(alvo, produtoAlvo)
+        elif select == '3':
+            listKeys()
+        elif select == '4':
+            listKeys()
+            alvo = input("Insira o Email da pessoa: ")
+            removerUser(alvo)
+        elif select == '5':
+            listKeys()
+            alvo = input("Insira o Email da pessoa: ")
+            listFavoritosUser(alvo)
+        elif select == '6':
+            listKeys()
+            alvo = input("Insira o Email da pessoa: ")
+            sincronizarRedisMongo(alvo)
+        elif select == '7':
+            listKeys()
+            alvo = input("Insira o Email da pessoa: ")
+            loginRedis(alvo)
+        elif select == "X":
+            on = False
+            main()
+        elif select == "cls":
+            clearConsole()
+            return redisAtv()
+        else:
+            print('Opção não entendida, por favor digite novamente')
 
 
 main()
+
+
+def testes():
+    login = f'logged:{alvo}'
+    timeToLeft = conR.ttl(login)
+    if timeToLeft <= -1:
+        print("Deslogdado")
+        conR.hset(f'logged:{alvo}', 'timeExit', 0)
+        conR.hset(f'user:{alvo}', 'loggin', 'off')
+        efetuarLogin = input("Deseja logar? S/N? ")
+        if efetuarLogin == 'S':
+            conR.expire('logged:bicho@nunes', 15)
+        else:
+            print("Saindo...")
+    else:
+        print("Logado")
+        conR.hset(f'logged:{alvo}', 'loggin', 'on')
+
+
+""" print(conR.keys())
+print(conR.ttl('logged:bicho@nunes')) """
+""" testes() """
+""" 
+    login = 'logged:bicho@nunes'
+    chaves = conR.keys()
+    keys = []
+    userLogin = conR.hget(f'user:' + 'bicho@nunes', 'loggin')
+    status = userLogin.decode()
+    for key in chaves:
+        keys.append(key.decode())
+    if login in keys:
+        if status == 'off':
+            print('Logando...')
+            conR.hset('user:bicho@nunes', 'loggin', 'on')
+        else:
+            conR.expire('logged:bicho@nunes', 15)
+            print('Você já está logado!')
+    else:
+        conR.hset('logged:bicho@nunes', 'timeExit', 0)
+        print('Sessão expirada')
+
+
+
+if 
+    if status == 'off':
+        conR.hset('logged:bicho@nunes', 'timeExit', 0)
+        conR.expire('logged:bicho@nunes', 30)
+        conR.hset('user:bicho@nunes', 'loggin', 'on')
+    else:
+        print('Você já está logado')
+
+    userLogin = conR.hget(f'user:' + 'bicho@nunes', 'loggin')
+    status = userLogin.decode()
+    time_to_expire_s = 10
+    for i in range(10):
+        conR.hset('loggin:' + 'bicho@nunes', 'logged',
+                 json.dumps({
+                     'value': i,
+                     'expiration': time_to_expire_s
+                 })) """
